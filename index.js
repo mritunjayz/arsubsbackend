bfa7ab38583022928713df60a24dabff25d4fda4
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2022-07-23 23:29:23 +0530

    change implementation

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+const port = 3000
 const http = require('http'); // Loads the http module 
 const axios = require('axios');
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
 
-console.log('llllll')
+app.use(bodyParser.urlencoded({ extended: false }))
 
-http.createServer((request, response) => {
-    
-    // 1. Tell the browser everything is OK (Status code 200), and the data is in plain text
-    response.writeHead(200, {
-        'Content-Type': 'application/json; charset=utf-8'
-    });
-    console.log(request,'reqqqq')
+// parse application/json
+app.use(bodyParser.json())
 
-    // 2. Write the announced text to the body of the page
+app.post('/', (req, res) => {
+  //res.send('Hello World!')
+  console.log(req.body, req.data, req)
+//   res.writeHead(200, {
+//     'Content-Type': 'application/json; charset=utf-8'
+// });
 
-    axios
+
+  axios
   .post('https://api-translate.systran.net/translation/text/translate', {
-    input: 'Buy the milk',
+    input: req.body.text,
     target: 'hi',
   }, {
     headers: {
       Authorization: 'Key f7c1bb3b-a7bb-4736-b4ce-73fd305eaf9c',
     }
   })
-  .then(res => {
+  .then(data => {
     //console.log(`statusCode: ${res.status}`);
     //console.log(res, res.data.outputs[0].output);
-    response.write(res.data.outputs[0].output);
-    response.end();
+    res.send(data.data.outputs[0].output);
+    //response.end();
   })
   .catch(error => {
     console.error(error);
   });
 
+})
 
-
-    // 3. Tell the server that all of the response headers and body have been sent
-    
-
-}).listen(process.env.PORT || 3000); // 4. Tells the server what port to be on
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${process.env.PORT || port}!`)
+})
